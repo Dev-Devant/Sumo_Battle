e59a7df8b4f99a0abca4f36938f46e46da216a2e
Dev-Devant <89264391+Dev-Devant@users.noreply.github.com>
2024-02-03 16:33:49 -0300

    Update clase 3/2/24
    agregamos interacciones con los powerups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour{
     private Rigidbody playerRb;
     public float Force = 2;
     private GameObject focal;
+    public bool hasPowerUp = false;
+    public GameObject VFXPowerup ; 
     void Start()   {
         playerRb = GetComponent<Rigidbody>();
         focal = GameObject.Find("FocalPoint");
@@ -16,9 +18,44 @@ public class PlayerController : MonoBehaviour{
     void Update()    {
         float Vimp = Input.GetAxis("Vertical");
         float vhor = Input.GetAxis("Horizontal");
-        Vector3 director = (Vector3.forward * Vimp + Vector3.left * vhor).normalized;
+        Vector3 director = (Vector3.forward * Vimp - Vector3.left * vhor);
+        playerRb.AddForce(director * Force * Time.deltaTime ,ForceMode.Impulse);
+    }
 
-        playerRb.AddForce(director * Force * Vimp,ForceMode.Force);
+    void OnTriggerEnter( Collider other ){
+        if (hasPowerUp) {
+            return;
+        }
+
+        if ( other.CompareTag("PowerUpBig")){
+            hasPowerUp = true;
+            Destroy( other.gameObject );
+            GameObject VFX = Instantiate(VFXPowerup,transform.position,VFXPowerup.transform.rotation);
+            gameObject.transform.localScale  *= 2;
+        }
+        if ( other.CompareTag("PowerUpSpeed")){
+            hasPowerUp = true;
+            Destroy( other.gameObject );
+            GameObject VFX = Instantiate(VFXPowerup,transform.position,VFXPowerup.transform.rotation);
+            Force  *= 1.5f;
+        }
+        if ( other.CompareTag("PowerUpForce")){
+            hasPowerUp = true;
+            Destroy( other.gameObject );
+            GameObject VFX = Instantiate(VFXPowerup,transform.position,VFXPowerup.transform.rotation);
+            Force  *= 1.5f;
+        }
+
+    }
+
+    void OnCollisionEnter (Collision other){
+        if (other.gameObject.CompareTag("Enemy")) {
+            Rigidbody enemyRb= other.gameObject.GetComponent<Rigidbody>();
+            Vector3 dir = (other.transform.position - transform.position);
+            enemyRb.AddForce(Normalize(dir) * 10 , ForceMode.Impulse);
+        }
+
+
 
     }
 }
